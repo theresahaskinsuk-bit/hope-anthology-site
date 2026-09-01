6ae1632a3cba5f844ff87797b9341bfecaa11fc9
theresahaskinsuk-bit <theresahaskinsuk@gmail.com>
2026-09-01 09:23:32 +0100

    Add files via upload

--- content.home.js
+++ content.home.js
@@ -7,7 +7,7 @@ window.HA_HOME_CONTENT = {
   images: {
     logo: 'https://images.squarespace-cdn.com/content/6a258894c750534b28845855/e789bc30-2f5c-4037-8190-a9355d3a8d20/home-logo-hope-anthology.jpg?content-type=image%2Fjpeg',
     star: 'https://images.squarespace-cdn.com/content/6a258894c750534b28845855/1852dfa4-ea60-4e93-83b5-ac8ea2f0498d/home-footer-star-hope-anthology.png?content-type=image%2Fpng',
-    hero: 'https://images.squarespace-cdn.com/content/6a258894c750534b28845855/87960798-d30e-4aeb-ac66-ac81f28e65e5/the-hope-anthology-art-for-the-everyday.jpg?content-type=image%2Fjpeg',
+    hero: 'https://images.squarespace-cdn.com/content/6a258894c750534b28845855/fe3eea21-3b16-4747-b1e5-c2a958dd9dc9/the-hope-anthology-art-for-the-everyday-human.jpg?content-type=image%2Fjpeg',
     keep: 'https://images.squarespace-cdn.com/content/6a258894c750534b28845855/18cea152-1a12-4535-8cef-3099aab45bd5/home-panel-to-keep-symbolic-art.jpg?content-type=image%2Fjpeg',
     make: 'https://images.squarespace-cdn.com/content/6a258894c750534b28845855/f8b5d4d6-304b-4edf-aa13-4c87597d4873/home-panel-to-make-stained-glass-patterns-luna.jpg?content-type=image%2Fjpeg',
     why: 'https://images.squarespace-cdn.com/content/6a258894c750534b28845855/5171904f-5f1a-405f-80af-ff1199448f4e/home-panel-story-family-making-table.jpg?content-type=image%2Fjpeg'
